dec307cbe8e606317d3cab9e3fe7bdf9fe450035
jlessa <jefferson2459@gmail.com>
2022-03-14 15:24:13 -0300

    red icon

--- AvisoRadioLayer.js
+++ AvisoRadioLayer.js
@@ -13,7 +13,19 @@ function AvisoRadioLayer(props) {
     opacity: 0.6,
   };
 
+  var redIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
   var avisos = L.geoJSON(props.geoJSON, {
+    pointToLayer: function(feature,latlng){
+      return L.marker(latlng,{icon: redIcon});
+    },
     onEachFeature: function (aviso, layer) {
       layer.bindPopup(
         '<h3>' +
